fe0d9c6c6b5eff80a5ef3e4f709ebd2456af80d4
SHIMANTA DAS <shimanta.das@weavers-web.com>
2025-01-09 18:04:06 +0530

    first commit

---
+++ multi-posts.php
@@ -0,0 +1,22 @@
+<?php 
+
+/*
+ * Plugin Name:       Multi Site Posts
+ * Plugin URI:        https://example.com/plugins/the-basics/
+ * Description:       Handle the basics with this plugin.
+ * Version:           1.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Shimanta Das
+ * Author URI:        https://author.example.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://example.com/my-plugin/
+ * Text Domain:       multi-site-posts, 
+ */
+
+ if (!defined('ABSPATH')) {
+    die("You are restricted to access this page!");
+ }
+ 
+?>
